711051745ef0124c51cd7f0ebd683b9b2e9cba3c
Legendahkiin <frederikhensen@hotmail.com>
2015-07-18 19:42:58 +0200

    Support for /SERVERS + hides command
    string.lower(text)
    return "";

--- lua/autorun/server/serverlist.lua
+++ lua/autorun/server/serverlist.lua
@@ -1,6 +1,8 @@
 function ISaid( ply, text, public )
+    string.lower(text)
     if (string.sub(text, 1) == "/servers") then
         RunConsoleCommand("serverlist");
+        return "";
     end
 end
 hook.Add( "PlayerSay", "ISaid", ISaid );
