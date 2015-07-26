5e823912ab88abfe9c9d4062dde2bf43a917225f
Legendahkiin <frederikhensen@hotmail.com>
2015-07-26 17:27:28 +0200

    Serverlist 2.0

--- lua/autorun/client/serverlist.lua
+++ lua/autorun/client/serverlist.lua
@@ -1,11 +1,10 @@
-concommand.Add("serverlist", function(ply)
+-- Serverlist 2.0
+-- Created by Legendahkiin
 
-// Frame configurations
-
+concommand.Add("serverlist", function(ply) -- Upon command call, do as follows
 	local Frame = vgui.Create("DFrame")
 		Frame:SetTitle("Trippy Gaming Servers")
-		Frame:SetSize(ScrW() * 0.12, ScrH() * 0.4)
-		//Frame:SetSize(225, 450)
+		Frame:SetSize(ScrW() * 0.12, ScrH() * 0.4) -- Need to make an automatic function of this
 		Frame:SetPos(ScrW() * 0.05, ScrH() * 0.1)
 		Frame:MakePopup()
 		Frame.Paint = function(self, w, h)
@@ -14,124 +13,45 @@ concommand.Add("serverlist", function(ply)
 		Frame.btnMaxim:Hide()
 		Frame.btnMinim:Hide()
 
-// Dark RP configurations
+	-- Height = button height
+	-- iheight = ip height
+	local servers = {
+		{ name = "DarkRP", ip = "108.61.198.226", port = 27030, height = (ScrH() * 0.03), iheight = (ScrH() * 0.06)},
+		{ name = "Deathrun", ip = "108.61.198.226", port = 27015, height = (ScrH() *  0.1), iheight = (ScrH() * 0.13)},
+		{ name = "Prop Hunt", ip = "108.61.198.226", port = 27035, height = (ScrH() * 0.17), iheight = (ScrH() * 0.20)},
+		{ name = "TTT", ip = "108.61.198.226", port = 27017, height = (ScrH() * 0.24), iheight = (ScrH() * 0.27)},
+		{ name = "Murder", ip = "108.61.198.226", port = 27019, height = (ScrH() * 0.31), iheight = (ScrH() * 0.34)}
+	}
 
-	local RPButton = vgui.Create("DButton", Frame)
-		RPButton:SetText("JOIN DARK-RP")
-		RPButton:SetTextColor(Color(255, 255, 255 ))
-		RPButton:SetPos(ScrW() * 0.0325, ScrH() * 0.03)
-		RPButton:SetSize(100, 30)
-		RPButton.Paint = function(self, w, h)
+	for k, server in pairs ( servers ) do
+		local name = server.name
+		local ip = server.ip
+		local port = server.port
+		local height = server.height
+		local iheight = server.iheight
+
+		local button = vgui.Create( "DButton", Frame )
+		button:SetText( "Join ".. name )
+		button.DoClick = function()
+			LocalPlayer():ConCommand( "connect " .. ip .. ":" .. port )
+		end
+
+		button:SetSize(100, 30)
+		button:SetPos(ScrW() * 0.0325, height)
+		button:SetTextColor(Color(255, 255, 255 ))
+		button.Paint = function(self, w, h)
 			draw.RoundedBox(0, 0, 0, w, h, Color(41, 128, 185, 250))
 		end
-		RPButton.DoClick = function()
-			LocalPlayer():ConCommand("connect 108.61.198.226:27030")
-		end
 
-	local RPA = vgui.Create("DLabel", Frame)
-		RPA:SetText("108.61.198.226:27030")
-		RPA:SetPos(ScrW() * 0.0305, ScrH() * 0.06)
-		RPA:SizeToContents()
-		RPA:SetMouseInputEnabled(true)
-		function RPA:DoClick()
-			SetClipboardText("108.61.198.226:27030")
+		local label = vgui.Create("DLabel", Frame)
+		label:SetText( ip.. ":" .. port )
+		label:SetPos(ScrW() * 0.0305, iheight)
+		label:SetMouseInputEnabled(true)
+		label:SizeToContents()
+		function label:DoClick()
+			SetClipboardText( ip .. ":" .. port)
 		end
-		RPA:SetTooltip("Click to copy!")
+		label:SetTooltip("Click to copy!")
 
-// Deathrun configurations
-
-	local DRButton = vgui.Create("DButton", Frame)
-		DRButton:SetText("JOIN DEATHRUN")
-		DRButton:SetTextColor(Color(255, 255, 255 ))
-		DRButton:SetPos(ScrW() * 0.0325, ScrH() * 0.1)
-		DRButton:SetSize(100, 30)
-		DRButton.Paint = function(self, w, h)
-			draw.RoundedBox(0, 0, 0, w, h, Color(41, 128, 185, 250))
-		end
-		DRButton.DoClick = function()
-			LocalPlayer():ConCommand("connect 108.61.198.226:27015")
-		end
-
-	local DRA = vgui.Create("DLabel", Frame)
-		DRA:SetText("108.61.198.226:27015")
-		DRA:SetPos(ScrW() * 0.0305, ScrH() * 0.13)
-		DRA:SizeToContents()
-		DRA:SetMouseInputEnabled(true)
-		function DRA:DoClick()
-			SetClipboardText("108.61.198.226:27015")
-		end
-		DRA:SetTooltip("Click to copy!")
-
-// Prophunt configurations
-
-	local PHButton = vgui.Create("DButton", Frame)
-		PHButton:SetText("JOIN PROPHUNT")
-		PHButton:SetTextColor(Color(255, 255, 255 ))
-		PHButton:SetPos(ScrW() * 0.0325, ScrH() * 0.17)
-		PHButton:SetSize(100, 30)
-		PHButton.Paint = function(self, w, h)
-			draw.RoundedBox(0, 0, 0, w, h, Color(41, 128, 185, 250))
-		end
-		PHButton.DoClick = function()
-			LocalPlayer():ConCommand("connect 108.61.198.226:27035")
-		end
-
-	local PHA = vgui.Create("DLabel", Frame)
-		PHA:SetText("108.61.198.226:27035")
-		PHA:SetPos(ScrW() * 0.0305, ScrH() * 0.2)
-		PHA:SizeToContents()
-		PHA:SetMouseInputEnabled(true)
-		function PHA:DoClick()
-			SetClipboardText("108.61.198.226:27035")
-		end
-		PHA:SetTooltip("Click to copy!")
-
-// TTT configurations
-
-	local TTTButton = vgui.Create("DButton", Frame)
-		TTTButton:SetText("JOIN TTT")
-		TTTButton:SetTextColor(Color(255, 255, 255 ))
-		TTTButton:SetPos(ScrW() * 0.0325, ScrH() * 0.24)
-		TTTButton:SetSize(100, 30)
-		TTTButton.Paint = function(self, w, h)
-			draw.RoundedBox(0, 0, 0, w, h, Color(41, 128, 185, 250))
-		end
-		TTTButton.DoClick = function()
-			LocalPlayer():ConCommand("connect 108.61.198.226:27017")
-		end
-
-	local TTTA = vgui.Create("DLabel", Frame)
-		TTTA:SetText("108.61.198.226:27017")
-		TTTA:SetPos(ScrW() * 0.0305, ScrH() * 0.27)
-		TTTA:SizeToContents()
-		TTTA:SetMouseInputEnabled(true)
-		function TTTA:DoClick()
-			SetClipboardText("108.61.198.226:27017")
-		end
-		TTTA:SetTooltip("Click to copy!")
-
-
-// Murder configurations
-
-	local MButton = vgui.Create("DButton", Frame)
-		MButton:SetText("JOIN MURDER")
-		MButton:SetTextColor(Color(255, 255, 255 ))
-		MButton:SetPos(ScrW() * 0.0325, ScrH() * 0.31)
-		MButton:SetSize(100, 30)
-		MButton.Paint = function(self, w, h)
-			draw.RoundedBox(0, 0, 0, w, h, Color(41, 128, 185, 250))
-		end
-		MButton.DoClick = function()
-			LocalPlayer():ConCommand("connect 108.61.198.226:27019")
-		end
-
-	local MA = vgui.Create("DLabel", Frame)
-		MA:SetText("108.61.198.226:27019")
-		MA:SetPos(ScrW() * 0.0305, ScrH() * 0.34)
-		MA:SizeToContents()
-		MA:SetMouseInputEnabled(true)
-		function MA:DoClick()
-			SetClipboardText("108.61.198.226:27019")
-		end
-		MA:SetTooltip("Click to copy!")
+	end
 end)
